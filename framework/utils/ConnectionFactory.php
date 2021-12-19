c01b4b07457d1e34cdc8603e958073e35b96b054
Youssef BAHI <you97ssef@gmail.com>
2021-12-19 20:15:52 +0100

    config database done

--- framework/utils/ConnectionFactory.php
+++ framework/utils/ConnectionFactory.php
@@ -8,7 +8,7 @@ class ConnectionFactory
 {
     private static $pdo;
 
-    public static function makeConnection(array $conf)
+    public static function makeConnection(array $conf, string $file = "")
     {
         $driver = $conf['driver'];
         $host = $conf['host'];
@@ -16,9 +16,12 @@ class ConnectionFactory
         $username = $conf['username'];
         $password = $conf['password'];
 
-        $dns = $driver . ':host=' . $host . ';dbname=' . $schema;
-
-        self::$pdo = new PDO($dns, $username, $password);
+        if ($driver == "sqlite")
+            self::$pdo = new PDO("sqlite:$file\\$schema.sqlite");
+        else {
+            $dns = $driver . ':host=' . $host . ';dbname=' . $schema;
+            self::$pdo = new PDO($dns, $username, $password);
+        }
     }
 
     public static function getConnection()
